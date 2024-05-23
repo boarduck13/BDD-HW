68b0783e0cd85c48a08f06a8e6b87b74799f700c
Andrey <justupor@gmail.com>
2024-05-23 22:42:27 +0300

    Add test

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,43 @@
+package data;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    public static class AuthInfo {
+        private String login;
+        private String password;
+        public AuthInfo(String login, String password) {
+            this.login = login;
+            this.password = password;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+    public static class VerificationCode {
+        private String code;
+
+        public VerificationCode(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+
+        return new VerificationCode("12345");
+    }
+
+}
